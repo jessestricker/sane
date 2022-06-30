2ecc36661fc142916d680b9abb862aa9802ad4d7
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2022-06-30 16:46:00 +0200

    docs: fix copyright formatting

--- src/sane/sane.hpp
+++ src/sane/sane.hpp
@@ -12,7 +12,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
 
 #ifndef SANE_SANE_HPP
 #define SANE_SANE_HPP
